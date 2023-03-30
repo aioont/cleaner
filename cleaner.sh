d580ea868b5bf08a0c96ca8e8f7a75f61e6136c7
aioont <aioont8@gmail.com>
2023-03-30 23:35:18 +0530

    Update cleaner.sh

--- cleaner.sh
+++ cleaner.sh
@@ -1,56 +1,64 @@
 #!/bin/bash
 
-# Prompt the user if they want to check free disk space
-read -p "Do you want to check free disk space? [y/n]: " answer
+read -p "Do you want to check free disk space ? [y/n] :" yn1
 
-case $answer in
-    [Yy]* )
-        df -H
-        ;;
-    [Nn]* )
-        ;;
-    * )
-        echo "Please answer yes or no."
-        exit 1
-        ;;
+case $yn1 in
+
+       [Yy]* ) df -H;;
+
+       [Nn]* ) unset yn1;;
+
+       *) echo "Please answer yes or no.";;
+
 esac
 
-# Prompt the user to enter the directory to search
-read -p "Enter directory to search or press enter for default [/data/data/com.termux/files]: " search_dir
+echo "Enter directory to search or press enter for default"
 
-# Use default directory if user doesn't enter anything
-if [ -z "$search_dir" ]; then
-    search_dir="/data/data/com.termux/files"
-fi
+dir="/data/data/com.termux/files"
 
-# Check if the directory exists
-if [ ! -d "$search_dir" ]; then
-    echo "Directory $search_dir does not exist"
-    exit 1
-fi
+read dir
 
-# Prompt the user for the number of largest folders to find
-read -p "How many largest folders need to find: " num
+cd $dir
 
-# Find the largest folders
-echo "$num largest folders name loading..."
-du -ha "$search_dir" | sort -k1hr | head -n "$num"
+echo 'How many largest folder needs to find : '
 
-# Prompt the user if they want to delete any files
-while true; do
-    read -p "Do you want to delete any files? [y/n]: " answer
-    case $answer in
-        [Yy]* )
-            ls -a "$search_dir"
-            read -p "Enter directory or folder or file to delete: " file_to_delete
-            rm -rf "$file_to_delete"
-            ls -a "$search_dir"
-            ;;
-        [Nn]* )
-            exit
-            ;;
-        * )
-            echo "Please answer yes or no."
-            ;;
+read num
+
+echo "$num largest files name loading ... : "
+
+du -ha | sort -k1hr | head -n $num
+
+while :
+
+do
+
+  read -p "Do you want to delete any [y/n] :" yn
+
+  case $yn in
+
+        [Yy]* ) ls -a;
+
+                echo "Enter directory or folder or file to delete : "
+
+                read deldir
+
+                rm -rf $deldir;
+
+                ls -a;;
+
+        [Nn]* ) exit;;
+
+        * ) echo "Please answer yes or no.";;
+
     esac
+
 done
+
+
+
+
+
+
+
+
+
