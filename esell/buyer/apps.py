19556b26954b5b0dd97507388c45df88a749ab8b
aioont <aioont8@gmail.com>
2023-04-01 23:36:22 +0530

    Delete esell directory

--- esell/buyer/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BuyerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'buyer'
--- esell/buyer/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-from django.template import loader
-
-def index(request):
-  return render(request,'index.html')
